965d4bd9a3dc07f7080bb3e207b251717c8f2422
Joel BE276 <joel.beauroy@outlook.fr>
2021-07-13 15:20:00 +0200

    first commit

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Users;
+use Symfony\Config\SecurityConfig;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+     private $passwordHasher;
+
+     public function __construct(UserPasswordHasherInterface $passwordHasher)
+     {
+        $this->passwordHasher = $passwordHasher;
+     }
+
+
+    public function load(ObjectManager $manager)
+    {
+           $user = new Users();
+           $user->setEmail("p.jordan@norimmo.com");
+           $user->setFirstname("Philippe");
+           $user->setLastname("Jordan");
+           $user->setPassword($this->passwordHasher->hashPassword(
+            $user,
+            '123456'
+           ));
+           $manager->persist($user);
+
+        $manager->flush();
+    }
+}
