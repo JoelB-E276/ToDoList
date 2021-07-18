d25fe77527934aa95698b9a10ea0b5bfe3271c70
JoelB-E276 <joel.beauroy@outlook.fr>
2021-07-18 23:03:40 +0200

    add NewtaskType

--- src/Controller/TasksController.php
+++ src/Controller/TasksController.php
@@ -7,7 +7,9 @@ use App\Entity\Task;
 use App\Entity\User;
 use App\Form\TaskType;
 use App\Form\ProjectType;
+use App\Form\NewTaskType;
 use App\Form\NewProjectType;
+use App\Repository\UsersRepository;
 use App\Repository\TaskRepository;
 use App\Repository\ProjectRepository;
 use Symfony\Component\HttpFoundation\Request;
@@ -53,28 +55,31 @@ class TasksController extends AbstractController
     }
 
 
-    //Créer un tâche ## En cours
-    #[Route('/tasks{id}', name: 'task_new', methods: ['GET', 'POST'])]
-    public function newTask(ProjectRepository $projectRepository, Request $request, Task $task, int $id): Response
+    //Créer un tâche 
+    #[Route('/project{id}', name: 'task_new', methods: ['GET', 'POST'])]
+    public function newTask(ProjectRepository $projectRepository, Request $request, int $id): Response
     {
-        $form = $this->createForm(TaskType::class, $task);
+        $task =new Task();
+        $form = $this->createForm(NewTaskType::class, $task);
         $form->handleRequest($request);
 
      if ($form->isSubmitted() && $form->isValid()) 
         {
+            $project = $projectRepository->find($id);
+            $task->setProject($project);
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($task);
             $entityManager->flush();
             $this->addFlash(
                 "success",
                 "Votre tâche a bien été créé"
-            );
+            ); 
 
 
             return $this->redirectToRoute('projects_index', [], Response::HTTP_SEE_OTHER);
-        }
+        } 
 
-        return $this->render('tasks/edit.html.twig', [
+        return $this->render('tasks/new.html.twig', [
             "task" => $task,
             "form" => $form->createView()
         ]);
